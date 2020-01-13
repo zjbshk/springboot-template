c849bf551f0bc1265e086c57cc47cc6f4c10ef53
占金彬 <oncwnuKss215Ng_Gw5KROX055GoQ@git.weixin.qq.com>
2020-01-13 11:41:13 +0800

    初始化

---
+++ src/main/java/cn/infomany/MainApplication.java
@@ -0,0 +1,17 @@
+package cn.infomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: selenium主类
+ * @author: zhanjinbing
+ * @data: 2020-01-13 11:15
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
